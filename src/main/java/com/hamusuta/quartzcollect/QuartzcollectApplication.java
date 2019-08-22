5698c9bc3739507d69933aa7ee8b2b1f60d757d1
jbw00 <525034005@qq.com>
2019-08-22 22:49:03 +0800

    info:初始化quartz collection demo

---
+++ src/main/java/com/hamusuta/quartzcollect/QuartzcollectApplication.java
@@ -0,0 +1,13 @@
+package com.hamusuta.quartzcollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzcollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzcollectApplication.class, args);
+	}
+
+}
